c42aeaf0e95bbd4e395483114575490ca892c0cc
dumindu2888 <dumindu2738@gmail.com>
2025-02-04 00:44:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ SESSION_ID: process.env.SESSION_ID || "LAKA-MD=11Zh2b4b#Rto4SwA3iv19DFBJA1-gIyRG
 //නැත්තම් bot deploy කරහම .settings comade එක use කරලත් වෙනස් කරගන්න පුලුවන්..🎊
 
 PREFIX: process.env.PREFIX || '.' ,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94781121579' : process.env.OWNER_NUMBER,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94775968322' : process.env.OWNER_NUMBER,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
 AUTO_REPLY: process.env.AUTO_REPLY === undefined ? "false" : process.env.AUTO_REPLY,
